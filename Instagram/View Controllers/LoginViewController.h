d6b5917bdd9696548e8e819b91499150ad309027
emokabe <emilyokb@stanford.edu>
2022-06-27 16:48:50 -0700

    Added user login/logout

---
+++ Instagram/View Controllers/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.h
+//  Instagram
+//
+//  Created by Emily Ito Okabe on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+- (IBAction)loginUser:(id)sender;
+
+- (IBAction)registerUser:(id)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
